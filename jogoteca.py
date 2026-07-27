13250628b393cb5cff5408b7c07f9ca4a273dabf
VMurtis <v.murtis@gmail.com>
2026-07-27 19:43:51 -0300

    corrige exibicao dos jogos acessando atributos no template

--- jogoteca.py
+++ jogoteca.py
@@ -8,11 +8,15 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+
+
+
 @app.route('/inicio')
 def ola():
     jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
     jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
-    lista = [jogo1, jogo2]
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
