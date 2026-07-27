23368d03974da06e25eae4f62c932f3e06d40e41
VMurtis <v.murtis@gmail.com>
2026-07-27 19:38:40 -0300

    adiciona classe Jogo e passa objetos para o template

--- jogoteca.py
+++ jogoteca.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+    lista = [jogo1, jogo2]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
