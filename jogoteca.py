60c8b322f280b401f87de543a6e76f958701eee7
VMurtis <v.murtis@gmail.com>
2026-07-27 23:10:26 -0300

    define rota principal como index e ativa modo debug

--- jogoteca.py
+++ jogoteca.py
@@ -13,8 +13,8 @@ jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
-@app.route('/inicio')
-def ola():
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
@@ -30,4 +30,4 @@ def criar():
     lista.append(jogo)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run()
+app.run(debug=True)
